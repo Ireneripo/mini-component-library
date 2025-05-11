d4ac3f5eccc207f9410c66b4dee9a24b1142a5e2
Irene Riportella <riportellairene@gmail.com>
2025-05-11 22:35:58 +0200

    feat:🎉 first commit

---
+++ src/components/ProgressBar/ProgressBar.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import styled from 'styled-components';
+
+import { COLORS } from '../../constants';
+import VisuallyHidden from '../VisuallyHidden';
+
+const ProgressBar = ({ value, size }) => {
+//  return <strong>{value}</strong>;
+
+    return <h1>HOLA</h1>
+};
+
+export default ProgressBar;
